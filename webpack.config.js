2865f9885325443a0e572f1e93341266a6c586f2
Mike Waterworth <mike.waterworth@epimorphics.com>
2018-02-14 13:54:07 +0000

    Add webpack file

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  entry: "./src/api-getter.js", // string | object | array
+  // Here the application starts executing
+  // and webpack starts bundling
+
+  output: {
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+
+    filename: "bundle.js", // string
+    // the filename template for entry chunks
+  }
+}
